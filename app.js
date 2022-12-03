48b5cb4baf6116c5354b57a5f165b6285cdaf8be
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-12-03 00:42:19 -0600

    Footer - HTML

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//setup date
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
 // setup nav
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
@@ -10,7 +13,3 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
-//setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
